24fa5b62385ec622b172f90afd6e823381ada6a3
sajib <me.mrsajib@gmail.com>
2022-06-16 21:43:32 +0600

    add meta tag pages

---
+++ pages/components/Meta.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+const Meta = ({ title, keyword, description }) => {
+  return (
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keyword} />
+
+      <title>{title}</title>
+    </Head>
+  );
+};
+defaultMeta = {
+  title: "News Room",
+  keyword: "English news, best news, today news, update news",
+  description: "Be updated news any country and any time",
+};
+export default Meta;
