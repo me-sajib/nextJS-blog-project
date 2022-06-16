018221aa4638fabc322b6e7a6285bb36b4e71373
sajib <me.mrsajib@gmail.com>
2022-06-16 20:54:01 +0600

    add to bootstrap

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h2>Hello Next js</h2>
+      <h2 className="text-center">Hello Next js</h2>
     </div>
   );
 }
